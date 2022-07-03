ae91c14b7821ee542f659eb7361f2b0f775d71f2
sai shankar <vsaishankar25@gmail.com>
2022-07-03 20:16:55 +0530

    initial setup and header done

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="p-5 text-3xl text-center overflow-x-auto">
+      <ul className="flex gap-10 items-center flex-nowrap font-bold">
+        <li>
+          <h1 className="whitespace-nowrap text-5xl"> {"</Sai Shankar>"}</h1>
+        </li>
+        <li className="ml-auto">hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+      </ul>
+    </div>
+  );
+};
